601d9bc4e23225eb67a58b4ad240cb8395c57b4f
Albert Florinus <46051169+AlbertFlorinus@users.noreply.github.com>
2019-02-16 12:17:33 +0100

    Add files via upload

---
+++ githubmnist.py
@@ -0,0 +1,11 @@
+import finalsteps
+import loadingscripts
+
+#model1 is the top performing model currently
+
+# command to preprocess images before classification is,
+# finalsteps.preprocessor(pather="where the folder is", writeto="where you want the preprocessed images located and folder named")
+
+loadingscripts.model1ui()
+
+#loadingscripts.model1(predict=True, pather="digits/siffrorsvåra")
