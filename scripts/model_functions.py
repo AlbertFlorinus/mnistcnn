1ba9d5167aabcbe8f5e5066253a8a16f49d7f52a
Albert Florinus <abbeflorinus@gmail.com>
2022-01-28 17:01:17 +0100

    refactoring

--- scripts/model_functions.py
+++ scripts/model_functions.py
@@ -1,23 +1,24 @@
-from tkinter.filedialog import askopenfilename
 from keras.preprocessing import image
 import numpy as np
 from keras.models import load_model
 import matplotlib.pyplot as plt
 import os
-#from cv2 import cv2
 import cv2
+
 # Creating class for the script
 class Run():
 	def __init__(self):
 		self.model = load_model("ALnet-3.0.h5")
+		"""
 		print("\nStructure of Alnet-3.0")
 		for layer in self.model.layers:
 			print(layer.output_shape)
+		"""
 	
-	def predict_chosen(self):
+	def predict_chosen(self, path_to_file):
 		# selecting image to predict
-		filename = askopenfilename()
-
+		#filename = askopenfilename()
+		filename = path_to_file
 		# reading image to grayscale
 		gray = cv2.imread(filename, 0)
 
@@ -73,6 +74,14 @@ class Run():
 		prediction_list = self.model.predict(test_img)
 		prediction2_list = self.model.predict(test_img2)
 
+		return {"prediction_list": prediction_list, "prediction2_list": prediction2_list, "test_img": test_img, "test_img2": test_img2}
+
+	def weighted_average(self, result):
+		prediction_list = result["prediction_list"]
+		prediction2_list = result["prediction2_list"]
+		test_img = result["test_img"]
+		test_img2 = result["test_img2"]
+
 		# x1 and x2 are indexes for elements (what digit is guessed) of highest value for images gray and gray2
 		x1 = np.argmax(prediction_list)
 		x2 = np.argmax(prediction2_list)
@@ -95,6 +104,31 @@ class Run():
 
 		if j > k:
 			classname = x2
+		return {"classname": classname, "adaptive_thresh_acc": k, "thresh_acc": j, "adaptive_thresh_img": test_img, "thresh_img": test_img2}
+
+	def predict_folder(self, pather, debug=False):
+		"""
+		pather is path to folder with images, 
+		returns a dict of dicts: {"file1": {"classname": , "adaptive_thresh_acc": , "thresh_acc": , "adaptive_thresh_img": , "thresh_img": }, "file2": {...}}
+		"""
+		dir = os.listdir(pather)
+		results = {}
+		for filename in dir:
+			if filename.endswith((".DS_Store")) or cv2.imread(f"{pather}/{filename}", 0) is None:
+				pass
+			else:
+				results[filename] = self.weighted_average(
+									self.predict_chosen(f"{pather}/{filename}")
+									)
+		return results
+
+	def plot_comparison(self, weighted_data):
+		# weighted data is same format as output of weighted_average()
+		test_img = weighted_data["adaptive_thresh_img"]
+		test_img2= weighted_data["thresh_img"]
+		classname = weighted_data["classname"]
+		k = weighted_data["adaptive_thresh_acc"]
+		j = weighted_data["thresh_acc"]
 
 		# reshaping again for showing image to user
 		test_img = test_img.reshape((112,112))
@@ -116,105 +150,3 @@ class Run():
 			fig.suptitle("Image shows a {}\n j > k".format(classname))
 
 		plt.show()
-
-		print("\nadaptive_Threshold prediction:\n",prediction_list,"\n")
-		print("Threshold prediction:\n",prediction2_list)
-		print("\nRunning: "+ "Alnet-3.0.")
-
-	def predict_folder(self, pather, debug=False):
-		
-		dir = os.listdir(pather)
-		corrects = []
-		wrongs = []
-		every = []
-		for filename in dir:
-			if filename.endswith((".DS_Store")):
-				pass
-			elif cv2.imread(f"{pather}/{filename}", 0) is None:
-				pass
-			else:
-				gray = cv2.imread(f"{pather}/{filename}", 0)
-				dilated_gray = cv2.dilate(gray, np.ones((7,7), np.uint8))
-				bg_gray = cv2.medianBlur(dilated_gray, 21)
-				diff_gray = 255 - cv2.absdiff(gray, bg_gray)
-				norm_gray = diff_gray.copy()
-				cv2.normalize(diff_gray, norm_gray, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
-
-				_, thr_gray = cv2.threshold(norm_gray, 253, 0, cv2.THRESH_TRUNC)
-				cv2.normalize(thr_gray, thr_gray, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
-				
-				gray1 = cv2.resize(255-thr_gray, (112, 112), interpolation=cv2.INTER_AREA)
-
-				gray = cv2.adaptiveThreshold(gray1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 5, 3)
-				(_, gray2) = cv2.threshold(gray1, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-				
-				img = image.img_to_array(gray)
-				img2 = image.img_to_array(gray2)
-
-				test_img = img.reshape((1,112,112,1))
-				test_img2 = img2.reshape((1,112,112,1))
-
-				test_img = img.astype('float32')
-				test_img2 = img2.astype('float32')
-
-				test_img /= 255
-				test_img2 /= 255
-
-				test_img = np.expand_dims(test_img, axis=0)
-				test_img2 = np.expand_dims(test_img2, axis=0)
-
-				prediction = self.model.predict(test_img)
-				prediction2 = self.model.predict(test_img2)
-
-				x1 = np.argmax(prediction)
-				x2 = np.argmax(prediction2)
-
-				c = prediction[0][x1]
-				v = prediction2[0][x2]
-
-				b = prediction[0][x2]
-				n = prediction2[0][x1]
-
-				k = (c+b)/2
-				j = (v+n)/2
-				if k > j:
-					classname = x1
-					
-				if j > k:
-					classname = x2
-				
-				# This is for benchmark tests during development and demonstrating larger sets of images,
-				# This requires the images to be labeled to see how many predictions were right or wrong,
-				# THIS IS OPTIONAL, if you dont label your images you wont get a list of prediction accuracy,
-				# But the predictions will be printed as outputs next to each image filename.
-				print(filename+ " prediction: ", classname)
-				if debug == True:
-					every.append(filename)
-					if filename.startswith(str(classname)):
-						corrects.append(filename)
-					else:
-						wrongs.append(filename)
-
-		if debug == True:
-			co = 0
-			w = 0
-			e = 0
-			total = len(every)
-			right = len(corrects)
-			wrong = len(wrongs)
-
-			print("all numbers")
-			print(e)
-			print("right guess")
-			print(co)
-			print("wrong guess")
-			print(w)
-			print("Wrong classifications: \n")
-			print(wrongs)
-			print("Running: "+ "ALnet-3.0")
-
-k = Run()
-#print(os.path.abspath(""))
-pather = os.path.abspath("")+"/digits"
-k.predict_folder(pather)
-
